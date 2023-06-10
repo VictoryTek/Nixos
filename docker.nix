c4732fe5d29eba125b1ef80f0efd8c79af0f9c3d
VictoryLinux <63359543+VictoryLinux@users.noreply.github.com>
2023-06-10 16:38:55 -0500

    Update docker.nix

--- docker.nix
+++ docker.nix
@@ -1,1 +1,31 @@
+{ config, pkgs, ... }:
 
+{
+
+  # Enable dconf (System Management Tool)
+  programs.dconf.enable = true;
+
+  # Add user to libvirtd group
+  users.users.nimda.extraGroups = [ "libvirtd" ];
+
+  # Install necessary packages
+  environment.systemPackages = with pkgs; [
+    docker
+    
+  ];
+
+  # Manage the virtualisation services
+  virtualisation = {
+    libvirtd = {
+      enable = true;
+      qemu = {
+        swtpm.enable = true;
+        ovmf.enable = true;
+        ovmf.packages = [ pkgs.OVMFFull.fd ];
+      };
+    };
+    spiceUSBRedirection.enable = true;
+  };
+  services.spice-vdagentd.enable = true;
+
+}
