eb0733f6e65e8d89c32d64809d0a88f4ff64c234
VictoryLinux <63359543+VictoryLinux@users.noreply.github.com>
2023-06-10 16:43:00 -0500

    Update docker.nix

--- docker.nix
+++ docker.nix
@@ -2,16 +2,16 @@
 
 {
 
-  # Enable dconf (System Management Tool)
-  programs.dconf.enable = true;
+  # Docker setup
+  virtualisation.docker.enable = true;
 
   # Add user to libvirtd group
-  users.users.nimda.extraGroups = [ "libvirtd" ];
+  users.users.nimda.extraGroups = [ "docker" ];
 
   # Install necessary packages
   environment.systemPackages = with pkgs; [
     docker
-    
+    docker-compose
   ];
 
   # Manage the virtualisation services
