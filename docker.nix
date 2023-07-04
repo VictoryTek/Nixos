a9478b345794de47e3a1e5d5ce321429aa289299
VictoryTek <136202114+VictoryTek@users.noreply.github.com>
2023-07-03 19:22:25 -0500

    Update docker.nix

--- docker.nix
+++ docker.nix
@@ -8,4 +8,10 @@
   # Add user to libvirtd group
   users.users.nimda.extraGroups = [ "docker" ];
 
+  # Install necessary packages
+  environment.systemPackages = with pkgs; [
+    pkgs.docker
+    pkgs.docker-compose
+  ];
+
 }
