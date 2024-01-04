0efadcc69c8c16cfa47a805f2afe4da7243851f0
VictoryTek <136202114+VictoryTek@users.noreply.github.com>
2024-01-04 15:11:42 -0600

    Update docker.nix

--- docker.nix
+++ docker.nix
@@ -1,17 +1,34 @@
+# Docker Configuration
+# https://github.com/VictoryTek
+# Ver. 1.1
+
 { config, pkgs, ... }:
 
 {
 
   # Docker setup
-  virtualisation.docker.enable = true;
+  virtualisation = {
+    docker = {
+      enable = true;
+      autoPrune = {
+        enable = true;
+        dates = "weekly";
+      };
+    };
+  };
 
-  # Add user to libvirtd group
-  users.users.nimda.extraGroups = [ "docker" ];
+  # Add user to docker group
+  users = {
+    users = {
+      nimda = {
+        extraGroups = [ "docker" ];
+      };
+    };
+  };
 
   # Install necessary packages
   environment.systemPackages = with pkgs; [
-    pkgs.docker
-    pkgs.docker-compose
+  docker-compose
   ];
 
 }
