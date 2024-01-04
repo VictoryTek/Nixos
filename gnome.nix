4f213697b2e16a3eebb8c831e5a6cc12167f8933
VictoryTek <136202114+VictoryTek@users.noreply.github.com>
2024-01-04 14:56:45 -0600

    Create gnome.nix

---
+++ gnome.nix
@@ -0,0 +1,88 @@
+# Gnome Configuration
+# https://github.com/VictoryTek
+# Ver. 1.0
+
+{ config, pkgs, ... }:
+
+{
+
+  # Enable the GNOME Desktop Environment.
+  services = {
+    xserver = {
+      enable = true;
+      displayManager = {
+        gdm = {
+          enable = true;
+        };
+        autoLogin = {
+          enable = true;
+          user = "nimda";
+        };
+      };
+      desktopManager = {
+        gnome = {
+          enable =true;
+        };
+      };
+    };
+  };
+
+  # Workaround for GNOME autologin & Disable the GNOME3/GDM auto-suspend feature that cannot be disabled in GUI: (https://github.com/NixOS/nixpkgs/issues/103746#issueco>)
+  systemd = {
+    services = {
+      "getty@tty1" = {
+        enable = false;
+      };
+      "autovt@tty1" = {
+        enable = false;
+      };
+    };
+    targets = {
+      sleep = {
+        enable = false;
+      };
+      suspend = {
+        enable = false;
+      };
+      hibernate = {
+        enable = false;
+      };
+      hybrid-sleep = {
+        enable = false;
+      };
+    };
+  };
+
+  # Packages to be removed
+  environment = {
+    gnome = {
+      excludePackages = (with pkgs; [
+        gnome-photos
+        gnome-tour
+      ]) ++ (with pkgs.gnome; [
+        cheese # webcam tool
+        gnome-music
+        epiphany # web browser
+        geary # email reader
+        evince # document viewer
+        gnome-characters
+        totem # video player
+        tali # poker game
+        iagno # go game
+        hitori # sudoku game
+        atomix # puzzle game
+        gnome-maps
+        gnome-clocks
+        gnome-weather
+      ]);
+    };
+  };
+
+  # Install necessary packages
+  environment = {
+    systemPackages = with pkgs; [
+        gnome.gnome-tweaks
+    ];
+  };
+  
+}
