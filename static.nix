864b4bde1757d8a702a2844c5102e46a029ace4a
VictoryTek <136202114+VictoryTek@users.noreply.github.com>
2024-01-04 15:06:43 -0600

    Create static.nix

---
+++ static.nix
@@ -0,0 +1,47 @@
+# STATIC Network Configuration
+# https://github.com/VictoryTek
+# Ver. 1.0
+
+{ config, pkgs, ... }:
+
+{
+
+  # Enable STATIC Networking.
+  networking = {
+    firewall.enable = false;
+    useDHCP = false;
+    interfaces = {
+      enp0s3 = {
+        ipv4.addresses = [ {
+          address = "192.168.100.21";
+          prefixLength = 21;
+        } ];
+      };
+    };
+    defaultGateway = "192.168.100.1";
+    nameservers = [ "192.168.100.1" ];
+  };
+
+
+
+  #Enable STATIC Networking with bridge.
+#  networking = {
+#    useDHCP = false;
+#    bridges = {
+#      "br0" = {
+#        interfaces = [ "eno1" ];
+#      }
+#    }
+#    interfaces = {
+#      br0 = {
+#        ipv4.addresses = [ {
+#          address = "192.168.100.21";
+#          prefixLength = 24;
+#        } ];
+#      };
+#    };
+#    defaultGateway = "192.168.100.1";
+#    nameservers = [ "192.168.100.1" ];
+#  }
+  
+}
