8f710e4074a54140a47f83b7ff07438f3d404b0f
VictoryTek <136202114+VictoryTek@users.noreply.github.com>
2024-01-04 15:11:00 -0600

    Create flakes-enabled.nix

---
+++ flakes-enabled.nix
@@ -0,0 +1,32 @@
+# Flakes enabling Configuration
+# https://github.com/VictoryTek
+# Ver. 1.0
+
+{ config, pkgs, ... }:
+
+{
+
+  # Enable Flakes and the new command-line tool
+  nix = {
+    settings = {
+      experimental-features = [ "nix-command" "flakes" ];
+    };
+  };
+
+  environment = {
+    systemPackages = with pkgs; [
+    # Flakes use Git to pull dependencies from data sources 
+    git
+    nano
+    wget
+    curl
+    ];
+  };
+  # Set default editor to vim
+  environment = {
+    variables = {
+      EDITOR = "nano";
+    };
+  };
+
+}
