2378775be64ac134283925b2affb699e1eb6cbd1
VictoryTek <136202114+VictoryTek@users.noreply.github.com>
2024-01-05 10:19:52 -0600

    Added modules

---
+++ modules/traefik.nix
@@ -0,0 +1,12 @@
+# Traefik Reverse Proxy Configuration
+# https://github.com/VictoryTek
+# Ver. 1.0
+
+{ config, pkgs, ... }:
+
+{
+
+  # Enable DHCP Networking
+  networking.networkmanager.enable = true;
+
+}
