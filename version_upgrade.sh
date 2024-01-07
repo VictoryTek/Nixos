ec2d72e0ffb05dee97eff623a17572f483e10247
VictoryLinux <63359543+VictoryLinux@users.noreply.github.com>
2024-01-06 22:28:32 -0600

    fix an error

--- version_upgrade.sh
+++ version_upgrade.sh
@@ -31,7 +31,7 @@ check_exit_status() {
 		
 		read -p "The last command exited with an error. Exit script? (y,Y/n,N) " answer
 
-            if [ "$answer" == "y" ] || [ "$answer" == "Y" ] || [[ "$input" == "yes" ]] || [[ "$input" == "Yes" ]]
+            if [ "$answer" == "y" ]
             then
                 exit 1
             fi
@@ -58,7 +58,7 @@ echo -e "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 	   echo "Nothing was entered by the user"
 
 	# was it a y or a yes?
-	elif [[ "$input" == "y" ]] [[ "$input" == "Y" ]] || [[ "$input" == "yes" ]] || [[ "$input" == "Yes" ]]; then
+	elif [[ "$input" == "y" ]] || [[ "$input" == "yes" ]]; then
 
 	   echo "You replied $input, you are ready to start"
 	   echo
@@ -104,7 +104,7 @@ function update() {
 	   echo "Nothing was entered by the user"
 
 	# was it a y or a yes?
-	elif [[ "$input" == "y" ]] || [[ "$input" == "Y" ]] || [[ "$input" == "yes" ]] || [[ "$input" == "Yes" ]]; then
+	elif [[ "$input" == "y" ]] || [[ "$input" == "yes" ]]; then
 
 	   echo "You replied $input, you are ready to start"
 	   echo
@@ -144,7 +144,7 @@ function list() {
 	   echo "Nothing was entered by the user"
 
 	# was it a y or a yes?
-	elif [[ "$input" == "y" ]] || [[ "$input" == "Y" ]] || [[ "$input" == "yes" ]] || [[ "$input" == "Yes" ]]; then
+	elif [[ "$input" == "y" ]] || [[ "$input" == "yes" ]]; then
 
 	   echo "You replied $input, Let's upgrade NixOS"
 	   echo
@@ -186,7 +186,7 @@ function reboot() {
 	   echo "Nothing was entered by the user"
 
 	# was it a y or a yes?
-	elif [[ "$input" == "y" ]] || [[ "$input" == "Y" ]] || [[ "$input" == "yes" ]] || [[ "$input" == "Yes" ]]; then
+	elif [[ "$input" == "y" ]] || [[ "$input" == "yes" ]]; then
 
 	   echo "You replied $input, rebooting"
 	   echo
