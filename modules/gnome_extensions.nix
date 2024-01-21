002e18b237a28f98e50e8dc8e9eed00df29a234a
VictoryLinux <63359543+VictoryLinux@users.noreply.github.com>
2024-01-21 12:09:12 -0600

    Create gnome_extensions.nix

---
+++ modules/gnome_extensions.nix
@@ -0,0 +1,19 @@
+# Flatpak Configuration
+# https://github.com/VictoryTek
+# Ver. 1.0
+
+{ config, pkgs, ... }:
+
+{
+
+  # Enable Gnome Extensions.
+  environment.systemPackages = [
+    # Install Tiling Assistant exxtension
+    pkgs = {
+        gnomeExtensions = {
+            tiling-assistant;
+        };
+    };
+  ];
+
+}
