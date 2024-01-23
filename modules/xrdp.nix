b76626b80ce923978436399e0aac8ae4fbdb59b4
VictoryLinux <63359543+VictoryLinux@users.noreply.github.com>
2024-01-22 18:06:06 -0600

    Update xrdp.nix

--- modules/xrdp.nix
+++ modules/xrdp.nix
@@ -6,26 +6,36 @@
 
 {
 
-    services = {
-        xserver = {
-            enable = true;
-            displayManager = {
-                gdm = {
-                    enable = true;
-                }
-            };
-            desktopManager = {
-             gnome = {
-                 enable = true;
-             };     
-            };
-        };
-       xrdp = {
-           enable = true;
-           defaultWindowManager = "gnome-session";
-           openFirewall = true;
-        };
+    services.xserver.enable = true;
+    services.xserver.displayManager.gdm.enable = true;
+    services.xserver.desktopManager.gnome.enable = true;
 
-    };
+    services.xrdp.enable = true;
+    services.xrdp.defaultWindowManager = "gnome-session";
+    services.xrdp.openFirewall = true;
+
+# -------------------------------------------------------
+
+#    services = {
+#        xserver = {
+#            enable = true;
+#            displayManager = {
+#                gdm = {
+#                   enable = true;
+#                };
+#            };
+#            desktopManager = {
+#             gnome = {
+#                 enable = true;
+#             };     
+#            };
+#        };
+#       xrdp = {
+#           enable = true;
+#           defaultWindowManager = "gnome-session";
+#           openFirewall = true;
+#        };
+#
+#    };
 
 }
