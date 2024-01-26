9d3f1e4a4c7af89f2e89203fff9828430ff35ba6
VictoryTek <136202114+VictoryTek@users.noreply.github.com>
2024-01-26 09:03:44 -0600

    Update static.nix

--- modules/static.nix
+++ modules/static.nix
@@ -13,7 +13,7 @@
     interfaces = {
       enp0s3 = {
         ipv4.addresses = [ {
-          address = "192.168.100.21";
+          address = "192.168.100.1";
           prefixLength = 21;
         } ];
       };
@@ -24,6 +24,7 @@
 
 
 
+Old
   #Enable STATIC Networking with a bridge.
 #  networking.useDHCP = false;
 #  networking.bridges = {
@@ -32,12 +33,36 @@
 #    };
 #  };
 #  networking.interfaces.br0.ipv4.addresses = [ {
-#    address = "192.168.100.21";
+#    address = "192.168.100.1";
 #    prefixLength = 24;
 #  } ];
 #  networking.defaultGateway = "192.168.100.1";
 #  networking.nameservers = ["192.168.100.1"];
 
+
+
+New
+  #Enable STATIC Networking with bridge.
+#  networking = {
+#    useDHCP = false;
+#    bridges = {
+#      "br0" = {
+#        interfaces = [ "eno1" ];
+#      };
+#    };
+#    interfaces = {
+#      br0 = {
+#        ipv4.addresses = [ {
+#          address = "192.168.100.1";
+#          prefixLength = 24;
+#        } ];
+#      };
+#    };
+#    defaultGateway = "192.168.100.1";
+#    nameservers = [ "192.168.100.1" ];
+#  };
+#
+#};
 
   
 }
